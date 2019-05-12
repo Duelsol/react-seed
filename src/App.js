3b2d56dca4e0130685e6fcc4ea588f2936e7a69e
Duelsol <duelsol@sina.com>
2019-05-12 11:52:54 +0800

    将全局提示放到Layout。

--- src/App.js
+++ src/App.js
@@ -1,39 +1,15 @@
 import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { actions } from './redux/modules/app'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
-import { Modal } from 'antd'
 import Layout from './pages/layout/Layout'
 
-class App extends Component {
-  hideInvalidUserAlert = () => {
-    this.props.dispatch(actions.hideInvalidUserAlert())
-  }
-
+export default class App extends Component {
   render() {
     return (
       <div>
         <Router>
           <Route path="/" component={Layout} />
         </Router>
-        <Modal
-          title="提示"
-          visible={this.props.invalidUserAlertVisible}
-          keyboard={false}
-          maskClosable={false}
-          onCancel={this.hideInvalidUserAlert}
-        >
-          你已被登出，点击“确定”重新登录，或点击“取消”继续留在当前页面
-        </Modal>
       </div>
     )
   }
 }
-
-function mapStateToProps(state) {
-  return {
-    invalidUserAlertVisible: state.app.invalidUserAlertVisible
-  }
-}
-
-export default connect(mapStateToProps)(App)
--- src/pages/layout/Layout.js
+++ src/pages/layout/Layout.js
@@ -1,10 +1,36 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { actions } from './redux/modules/app'
+import { Modal } from 'antd'
 import Main from './components/Main'
 
-export default class Layout extends Component {
+class Layout extends Component {
+  hideInvalidUserAlert = () => {
+    this.props.dispatch(actions.hideInvalidUserAlert())
+  }
+
   render() {
     return (
-      <Main></Main>
+      <React.Fragment>
+        <Main></Main>
+        <Modal
+          title="提示"
+          visible={this.props.invalidUserAlertVisible}
+          keyboard={false}
+          maskClosable={false}
+          onCancel={this.hideInvalidUserAlert}
+        >
+          你已被登出，点击“确定”重新登录，或点击“取消”继续留在当前页面
+        </Modal>
+      </React.Fragment>
     )
   }
 }
+
+function mapStateToProps(state) {
+  return {
+    invalidUserAlertVisible: state.app.invalidUserAlertVisible
+  }
+}
+
+export default connect(mapStateToProps)(Layout)
